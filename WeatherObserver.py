72b612068c7e1b4eea92627be834c58d9f515311
Daniel Hlavačka <150837702+LittleDanny92@users.noreply.github.com>
2023-12-09 20:18:02 +0100

    Add files via upload
    Update of css_path - full path with using os library

--- WeatherObserver.py
+++ WeatherObserver.py
@@ -1,9 +1,11 @@
 ﻿# -*- coding: utf-8 -*-
 
 import sys
+import os
 from PyQt5 import (
     QtWidgets,
-    QtCore
+    QtCore,
+    
     )
     
 from mainwindow import MainWindow   
@@ -12,7 +14,8 @@ if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
 
     try:
-        css_file = QtCore.QFile("format.css")
+        css_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "format.css")
+        css_file = QtCore.QFile(css_path)
         if css_file.open(QtCore.QFile.ReadOnly | QtCore.QFile.Text):
             style = QtCore.QTextStream(css_file).readAll()
             css_file.close()
